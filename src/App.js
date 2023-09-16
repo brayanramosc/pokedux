b4735713905284e5b8905275f654d96cb10fc92f
brayan-cincoe <brayanrcaicedo@gmail.com>
2023-09-16 18:12:12 -0500

    style: add pokedex logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Col } from 'antd';
+
+import logo from './assets/images/pokedex_vector_logo.png';
 import 'antd/dist/reset.css';
 import './App.css';
 
@@ -8,6 +10,9 @@ import PokemonList from './components/PokemonList';
 function App() {
   return (
     <div className="App">
+      <Col span={6} offset={9}>
+        <img src={logo} alt='Pokedex' />
+      </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
